53b6ea089e451c5ac46042c08e158105c9ca6ce8
erasernoob <1490910338@qq.com>
2024-12-25 13:24:21 +0800

    feat: suppport the remove the item from the localstorage

--- 06-shoppingListProj/script.js
+++ 06-shoppingListProj/script.js
@@ -20,19 +20,31 @@ function fitlerItem(e) {
     })
 }
 
+function onClickItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')) {
+        console.log('in')
+        removeItem(e.target.parentElement.parentElement)
+    }
+    checkUI()
+}
+
+function removeItemFromStorage(item) {
+    const text = item.firstChild.textContent
+    let items = getItemsFromStorage()
+    const newItems = items.filter((e) => e !== text)
+     localStorage.setItem('items', JSON.stringify(newItems))
+}
 
 
-function removeItem(e) {
-    // e.currentTarget is the eventListener's object
-    // e.target is the acutal click object
-    // if (e.target.tagName != 'i') {
-    //     return
-    // }
-    const button = e.target.parentElement 
-    if (button.classList.contains('remove-item') && confirm('Are you sure?')) {
-        button.parentElement.remove()
-        checkUI()
-        return 
+
+function removeItem(item) {
+    if (item == null) return
+    if (confirm('Are You sure?')) {
+        // Delete from the DOM
+        item.remove()
+
+        // Delete the LocalStorage
+        removeItemFromStorage(item)
     }
 }
 
@@ -40,9 +52,12 @@ function clearItem(e) {
     // const childrenList = Array.from(itemList.children)
     const childrenList = [...itemList.children]
     confirm('Are your sure to clear it all?')
+    // remove from the DOM
     childrenList.forEach((e) => {
         e.remove()
     })
+    // remove the localStorage
+    localStorage.removeItem('items')
     checkUI()
     // e.target.style.display = 'none'
 }
@@ -62,21 +77,53 @@ function createIcon(className) {
     return icon
 }
 
-function addItem(e) {
+/**
+ * create a new Element for the DOM 
+ * @param {the new Items Text} item 
+ */
+function addItemToDOM(item) {
+    const li = document.createElement('li')
+    li.appendChild(document.createTextNode(item))
+
+    const button = createButton('remove-item btn-link text-red')
+    li.appendChild(button)
+
+    itemList.appendChild(li)
+}
+
+function getItemsFromStorage() {
+    let itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    if (itemsFromStorage == null) {
+        return []
+    }
+    return itemsFromStorage
+}
+
+/**
+ * add the new Item to the localStorage 
+ * @param {new Item} item 
+ */
+function addItemToStorage(item) {
+    let itemsFromStorage = getItemsFromStorage()
+
+    itemsFromStorage.push(item)
+    
+    // convert the array to JSON String
+    localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+}
+
+function onAddItemSubmit(e) {
     e.preventDefault()
     const newItem = itemInput.value
     if (newItem === '') {
         alert('Please add an item!')
         return
     } 
-    const li = document.createElement('li')
-    li.appendChild(document.createTextNode(newItem))
+    addItemToDOM(newItem)
 
-    const button = createButton('remove-item btn-link text-red')
-    li.appendChild(button)
-
-    itemList.appendChild(li)
+    addItemToStorage(newItem)
     checkUI()
+    // disPlayItems()
     itemInput.value = ''
 }
 
@@ -91,12 +138,23 @@ function checkUI() {
     }
 }
 
+function disPlayItems() {
+    const itemsFromStorage = getItemsFromStorage()
+    itemsFromStorage.forEach((text) => {
+        addItemToDOM(text)
+    })
+    checkUI()
+}
+
+// Initialize app 
+function init() {
 // Event Listeners
-itemForm.addEventListener('submit', addItem)
-itemList.addEventListener('click', removeItem)
-clearBtn.addEventListener('click', clearItem)
-filterInput.addEventListener('input', fitlerItem)
-
-
-checkUI()
+    itemForm.addEventListener('submit', onAddItemSubmit)
+    itemList.addEventListener('click', onClickItem)
+    clearBtn.addEventListener('click', clearItem)
+    filterInput.addEventListener('input', fitlerItem)
+    document.addEventListener('DOMContentLoaded', disPlayItems)
+    checkUI()
+}
+init()
 
